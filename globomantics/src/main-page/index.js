9e3589772590d687399c4cf2c4e5b513a5fac0d0
cdelmonico <chrisdelmonico79@gmail>
2020-04-22 18:21:42 -0600

    created header

---
+++ globomantics/src/main-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main-page.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle="Providing Houses Worldwide"/>
+    </div>
+  );
+}
+
+export default App;
