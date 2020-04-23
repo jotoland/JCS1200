38ea5ab7896f8b7062aca41223a2464509a32e73
talindsey69 <tomas4co@yahoo.com>
2020-04-22 18:21:43 -0600

    created header

---
+++ globomantics/src/main-page/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main-page.css';
+import Header from "./header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle="Providing Houses World Wide"/>
+     
+    </div>
+  );
+}
+
+export default App;
