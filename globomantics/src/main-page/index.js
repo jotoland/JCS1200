48f1c00c317497d63d6b8dc8f225f4fc7a3f70fa
Lindseytaylor90 <TaylorLindsey@Taylors-MacBook-Pro.local>
2020-04-22 18:24:42 -0600

    created heaeder

---
+++ globomantics/src/main-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main-page';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header subtitle="Providing Houses WorldWide"/>
+    </div>
+  );
+}
+
+export default App;
