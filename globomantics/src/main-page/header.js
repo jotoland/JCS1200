8ffad4fac69ddf4235bf6018b7928dc76b28cdf2
jtoland <jtoland3.14@gmail>
2020-04-27 16:47:44 -0600

    Adding featured house setup

--- globomantics/src/main-page/header.js
+++ globomantics/src/main-page/header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './GloboLogo.png';
 
-const Header = (/* props */) => (
+const Header = (props) => (
     <header className="row">
         <div className="col-md-5">
             <img src={logo} className="logo" alt="logo" />
         </div>
         <div className="col-md-7 mt-5 subtitle">
-                Hello world! {/* {props.subtitle} */}
+                {props.subtitle}
         </div>
     </header>
 );
--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './main-page.css';
 import Header from './header';
+import FeaturedHouse from './featured-house';
+// import HouseFilter from './house-filter';
+// import SearchResults from '../search-results';
+// import HouseDetail from '../house';
 
-function App() {
-  return (
-    <div className="container">
-      <Header subtitle="Providing Houses World Wide!"/>
-    </div>
-  );
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+
+  state = {}
+
+  componentDidMount() {
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+      // this.determineUniqueCountries();
+    })
+  }
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse =  this.allHouses[randomIndex];
+      this.setState({ featuredHouse });
+    };
+  }
+
+  // determineUniqueCountries = () => {
+  //   const countries = this.allHouses
+  //     ? Array.from(new Set(this.allHouses.map(h => h.country)))
+  //     : [];
+  //   countries.unshift(null);
+  //   this.setState({ countries });
+  // }
+
+  // filterHouses = (country) => {
+  //   this.setState({ activeHouse: null });
+  //   const filteredHouses = this.allHouses.filter((h) => h.country === country);
+  //   this.setState({ filteredHouses });
+  //   this.setState({ country });
+  // }
+
+  // setActiveHouse = (house) => {
+  //   this.setState({ activeHouse: house });
+  // }
+
+  render() {
+    let activeComponent = null;
+    // if (this.state.country)
+    // activeComponent = <SearchResults country={this.state.country}
+    //   filteredHouses={this.state.filteredHouses} setActiveHouse={this.setActiveHouse} />;
+    // if (this.state.activeHouse)
+    //   activeComponent = <HouseDetail house={this.state.activeHouse}/>;
+    // if (!activeComponent)
+    //   activeComponent = <FeaturedHouse house={this.state.featuredHouse} />;
+    return (
+      <div className="container">
+        <Header subtitle="Providing houses all over the world"/>
+        {/* <HouseFilter countries={this.state.countries} filterHouses={this.filterHouses} /> */}
+        {/*activeComponent */}
+        <FeaturedHouse house={this.state.featuredHouse} />
+      </div>
+    );
+  }
 }
 
 export default App;
