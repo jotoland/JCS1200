540a0864cd91403a6d98d3279dab46e6bad5c414
tpwelsh89 <tpwelsh89@gmail.com>
2020-04-29 16:49:05 -0600

    updated css & added houses.json

--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './main-page.css';
 import Header from './header';
 
-function App() {
-  return (
-    <div className="container">
-      <Header subtitle="Providing Houses Worldwide"/>
-    </div>
-  );
+class App extends Component() {
+  render(){
+
+    return (
+      <div className="container">
+        <Header subtitle="Providing Houses Worldwide"/>
+      </div>
+    );
+  }
 }
 
 export default App;
