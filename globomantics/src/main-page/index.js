d48f3011dc5f7b61b53a4941bd07b8730231868f
tpwelsh89 <tpwelsh89@gmail.com>
2020-04-29 18:30:17 -0600

    Created featured & determin house function

--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -3,6 +3,43 @@ import './main-page.css';
 import Header from './header';
 
 class App extends Component() {
+
+  state = {}
+
+  componentDidMount(){
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+    // mock ajax call 
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+    })
+  }
+
+  determineFeaturedHouse = () => {
+    if(this.allHouses){
+      let randomIndex = Math.floor(Math.random()*this.allHouses.length);
+      let featuredHouse = this.allHouses[randomIndex];
+      this.setState({
+        featuredHouse
+      });
+    }
+  }
+  
+  determineUniqueCountries = () => {
+   const countries = this.allHouses
+    ? Array.from(new Set(this.allHouses.map(h => h.country)))
+    : [];
+    countries.unshift(null);
+    this.setState({
+      countries
+    });
+  }
+
   render(){
 
     return (
