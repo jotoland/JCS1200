2c59a0ac1e7ab33a1551b02a1a2e8d693ee21116
talindsey69 <tomas4co@yahoo.com>
2020-07-21 18:07:17 -0600

    *.suo *.user *.sln.docstates
    [Dd]ebug/
    [Rr]elease/
    x64/
    [Bb]in/
    [Oo]bj/
    
    [Tt]est[Rr]esult*/
    [Bb]uild[Ll]og.*
    
    *_i.c
    *_p.c
    *_i.h
    *.ilk
    *.meta
    *.obj
    *.pch
    *.pdb
    *.pgc
    *.pgd
    *.rsp
    *.sbr
    *.tlb
    *.tli
    *.tlh
    *.tmp
    *.tmp_proj
    *.log
    *.vspscc
    *.vssscc
    .builds
    *.pidb
    *.log
    *.svclog
    *.scc
    
    ipch/
    *.aps
    *.ncb
    *.opensdf
    *.sdf
    *.cachefile
    
    *.psess
    *.vsp
    *.vspx
    
    *.gpState
    
    _ReSharper*/
    *.[Rr]e[Ss]harper
    *.DotSettings.user
    
    publish/
    
    *.Publish.xml
    *.pubxml
    *.azurePubxml
    
    packages/
    !packages/repositories.config
    
    csx/
    *.build.csdef
    
    AppPackages/
    
    sql/
    *.Cache
    ClientBin/
    [Ss]tyle[Cc]op.*
    ![Ss]tyle[Cc]op.targets
    ~$*
    *~
    *.dbmdl
    *.[Pp]ublish.xml
    
    *.publishsettings
    
    Generated_Code/
    
    _UpgradeReport_Files/
    Backup*/
    UpgradeLog*.XML
    UpgradeLog*.htm
    
    App_Data/*.mdf
    App_Data/*.ldf
    
    Thumbs.db
    ehthumbs.db
    
    Desktop.ini
    
    $RECYCLE.BIN/
    
    .DS_Store
    
    _NCrunch*

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -6,7 +6,11 @@ namespace CSharp_Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1.
+            Console.WriteLine("Hello: Thomas Lindsey");
+
+            //2.
+
         }
     }
 }
