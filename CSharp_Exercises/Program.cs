c9bd6936fbccf283585d163e951cb70b318fae60
cdelmonico <chrisdelmonico79@gmail>
2020-07-28 17:10:48 -0600

    worked on exercises

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -6,7 +6,27 @@ namespace CSharp_Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1.
+            // Console.WriteLine("Hello: Chris Delmonico");
+
+            // 2.
+            /*
+            int a = 1;
+            int b = 2;
+            int sum = a + b;
+            Console.WriteLine("1+2="+ sum);
+            */
+
+            // 3.
+            // Console.WriteLine("4/2="+ (4/2));
+
+            // 4. 
+            Console.WriteLine(-1+(4*6));
+            Console.WriteLine((35+5)%7);
+            Console.WriteLine(14+-4*6/11);
+            Console.WriteLine(2+15/6*1-7%2);
+
         }
+
     }
 }
