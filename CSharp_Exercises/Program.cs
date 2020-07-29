6a3b20830af7b0cf056488615a12eba3cf012a67
jtoland <jtoland3.14@gmail.com>
2020-07-28 18:28:28 -0600

    Added AboutMe component

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -7,9 +7,27 @@ namespace CSharp_Exercises
         static void Main(string[] args)
         {
             // 1.
-            Console.WriteLine("Hello: John Toland");
+            // Console.WriteLine("Hello: John Toland");
 
-            // 2.
+            // 5.
+            
+            int number1;
+            int number2;
+            int temp;
+            Console.Write("\nInput the First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : "+number1);
+            Console.Write("\nSecond Number : "+number2);
+            Console.Read();
+            
+            // 6
+            
         }
     }
 }
--- me/src/components/AboutMe.js
+++ me/src/components/AboutMe.js
@@ -4,8 +4,46 @@ class AboutMe extends Component {
     render() {
         return(
             <div id="AboutSection">
-
+                <section class="section is-medium">
+                    <div class="container">
+                        <div class="columns is-centered">
+                            <div class="column is-three-quarters">
+                                <h2 class="title">About Me</h2>
+                                {/*insert colored line here*/}
+                                <div class="columns is-8 is-three-quarters">
+                                    <figure class="image is-1by1">
+                                        <img src="" alt=""/>
+                                    </figure>
+                                </div>
+                                <div class="column" id="Intro">
+                                    <p>
+                                    Hello, My name is John G. Toland. I am currently a Full Stack Software Engineer at Reed Group a Gaurdian Company. 
+                                    My experience includes, but not limited to, front-end
+                                    web development, UI design, application interface development, 
+                                    and data management. I enjoy the everyday challenge and problem
+                                    solving opportunities that push me to continue learning about 
+                                    software development.   
+                                    </p>
+                                </div>
+                                <div class="column" id="Personal">
+                                    <h3>Hobbies of mine:</h3>
+                                    <br />
+                                    <ul>
+                                        <li>Camping and Fishing</li><br/>
+                                        <li>Disc Golf</li><br/>
+                                        <li>Traveling</li><br/>
+                                        <li>Bike Riding</li><br/>
+                                        <li>Swimming</li><br/>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         );
     }
 }
+
+// exports the file/class
+export default AboutMe;
