beec032121cc0d1e53400ee78b400951adc47af5
Lindseytaylor90 <tylindsey90@gmail.com>
2020-08-04 18:07:33 -0600

    completed problems6-10

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -10,8 +10,12 @@ namespace CSharp_Exercises
             //Console.WriteLine("Hello: Taylor Lindsey");
 
             // 2.
-            //Console.WriteLine(56+29);
-
+            /*
+            int a = 56;
+            int b = 29;
+            int c = a+b;
+            Console.WriteLine(c);
+            */
             //3. 
             //Console.WriteLine(650/2);
 
@@ -22,7 +26,8 @@ namespace CSharp_Exercises
             //Console.WriteLine(2 + 5 / 6* 1- 7 % 2);
         
             //5.
-                int number1, number2, temp;
+            /*    
+            int number1, number2, temp;
             Console.Write("\nInput the First number : ");
             number1 = int.Parse(Console.ReadLine());
             Console.Write("\nInput the Second Number : ");
@@ -34,6 +39,126 @@ namespace CSharp_Exercises
             Console.WriteLine("\nFirst Number : "+number1);
             Console.WriteLine("\nSecond Number : "+number2);
             Console.Read();
+            */
+            
+            //6.
+            /*
+
+            int number1, number2, number3; 
+            Console.WriteLine("Input the firt number to mulitiply:  ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the second number to multiply:  ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Inpt the third number to multiply:  ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+           
+           int result = number1 * number2 * number3; 
+           Console.WriteLine("Output: {0} * {1} * {2}  = {3}",
+                              number1,number2, number3, result);
+            */
+
+        //7.
+        /*
+        Console.Write("Enter a number: ");
+        int number1= Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter another number: ");
+        int number2= Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("{0} + {1} = {2}", number1, number2, number1+ number2);
+        Console.WriteLine("{0} - {1} = {2}", number1, number2, number1-number2);
+        Console.WriteLine("{0} * {1} ={2}", number1, number2, number1 *number2);
+        Console.WriteLine("{0} / {1} = {2}", number1, number2, number1/number2);
+        Console.WriteLine("{0} mod {1} ={2}", number1, number2, number1%number2);
+        */
+
+        //8.
+        /*
+        int x;
+        int result;
+
+        Console.WriteLine("Enter a number:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        result =x*0;
+        Console.WriteLine("The table is : {0} x{1} = {2}", x, 0, result);
+        result=x*1;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 1, result);
+        result=x*2;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 2, result);
+        result=x*3;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 3, result);
+        result=x*4;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 4, result);
+        result=x*5;
+        Console.WriteLine("             : {0} x {1} = {2}",  x, 5, result);
+        result=x*6;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 6, result);
+        result=x*7;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 7, result);
+        result=x*8;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 8, result);
+        result=x*9;
+        Console.WriteLine("             : {0} x {1} ={2}", x, 9, result);
+        result=x*10;
+        Console.WriteLine("             : {0} x {1} = {2}", x, 10, result);
+       */
+        
+        //9.
+        /*
+        double number1, number2, number3, number4;
+
+        Console.Write("enter the first number:");
+        number1=Convert.ToDouble(Console.ReadLine());
+         Console.Write("Enter the Second number: ");
+         number2=Convert.ToDouble(Console.ReadLine());
+         Console.Write("Enter  the Third number: ");
+         number3=Convert.ToDouble(Console.ReadLine());
+         Console.Write("Enter the fourth number:");
+         number4=Convert.ToDouble(Console.ReadLine());
+
+         double result = (number1 + number2 + number3 + number4)/4;
+         Console.WriteLine("The averange of {0}, {1}, {2}, {3}, is {4}",
+         number1, number2, number3, number4, result); 
+         */
+
+         //10.
+         /*
+         //decliration of my variables
+         int number1, number2, number3;
+         
+         //prompt the user 
+         Console.Write("Enter first number-");
+         // read in the line covert to int set equal to number 1
+         number1=Convert.ToInt32(Console.ReadLine());
+
+         Console.Write("Enter second number- ");
+         number2= Convert.ToInt32(Console.ReadLine());
+
+         Console.Write("Enter third number - ");
+         number3= Convert.ToInt32(Console.ReadLine());
+
+         Console.Write("Result of specified numbers {0}, {1} and {2}, (x=y)*z is {3} and x*y + y*z is {4}\n\n",
+          number1, number2, number3, ((number1+number2)*number3), (number1*number2+number2*number3));
+          */
+
+
+        
+        
+
+        
+
+
+
+
+
+
+                    
+        
+
+            
             
 
 
