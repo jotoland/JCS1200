87164f9ac752edfefa9c16be2a669b2307286942
jtoland <jtoland3.14@gmail.com>
2020-08-04 20:24:48 -0600

    Adding main component

--- me/src/App.js
+++ me/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import AboutMe from './components/AboutMe';
+import Main from './components/Main';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Main />
       <AboutMe />
     </div>
   );
