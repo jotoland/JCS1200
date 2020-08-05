73acf7547718d8f39c37340bccf713bd27b8af52
talindsey69 <tomas4co@yahoo.com>
2020-08-04 21:29:02 -0600

    worked on exercise added about me component

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -7,10 +7,35 @@ namespace CSharp_Exercises
         static void Main(string[] args)
         {
             // 1.
-            Console.WriteLine("Hello: Thomas Lindsey");
+        
+            //Console.WriteLine("Hello: Thomas Lindsey");
 
             //2.
+            //Console.WriteLine(32+37);
 
+            //3.
+            //Console.WriteLine(276/4);
+
+            //4.
+            //Console.WriteLine(-1 + 4 * 6);
+            //Console.WriteLine(( 35+ 5 ) % 7);
+            //Console.WriteLine(14 + -4 * 6 / 11);
+            //Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+            
+            //5.
+               int number1, number2, temp;
+            Console.Write("\nInput the First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.Write("\nAfter Swapping : ");
+            Console.Write("\nFirst Number : "+number1);
+            Console.Write("\nSecond Number : "+number2);
+            Console.Read();
+        
         }
     }
 }
