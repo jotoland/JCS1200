8cb5f7dc5e032be30b2e27d228efe6429e60c319
jtoland <jtoland3.14@gmail.com>
2020-08-13 22:14:48 -0600

    Created navBar

--- me/src/App.js
+++ me/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import AboutMe from './components/AboutMe';
+<<<<<<< Updated upstream
 import Main from './components/Main';
+=======
+import Navbar from './components/Navbar';
+>>>>>>> Stashed changes
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Main />
+<<<<<<< Updated upstream
       <AboutMe />
+=======
+      <Navbar />
+      <AboutMe />          
+>>>>>>> Stashed changes
     </div>
   );
 }
