559a2eb67f1a554b90075932d7bb61e20e464151
jtoland <jtoland3.14@gmail.com>
2020-08-13 20:13:01 -0600

    lorem ipsum

--- me/src/components/AboutMe.js
+++ me/src/components/AboutMe.js
@@ -20,7 +20,13 @@ class AboutMe extends Component {
                             </div>
                             <div class="column" id="Intro">
                                 <p>
-                                Hello, Intro goes here 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                                pariatur. Excepteur sint occaecat cupidatat non proident, 
+                                sunt in culpa qui officia deserunt mollit anim id est laborum. 
                                 </p>
                             </div>
                             <div class="column" id="Personal">
