00dbd2f503187584ccc5c9301f98329d7b62f237
Lindseytaylor90 <tylindsey90@gmail.com>
2020-08-24 20:05:29 -0600

    Finished skills, started Experience

--- me/src/App.js
+++ me/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutMe from './components/AboutMe';
 import Main from './components/Main';
 import Navbar from'./components/Navbar';
+import Skills from './components/Skills';
 import './App.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Main />
       <AboutMe />
       <Navbar />
+      <Skills />
     </div>
   );
 }
---
+++ me/src/components/Experience.js
@@ -0,0 +1,22 @@
+import React,{ Component } from 'react';
+//import ColoredLine from './ColoredLine';
+
+class Experience extends Component {
+    render() {
+        return(
+            <div id="ExpeienceSection">
+                <section class="section is-meduim">
+                <div class="contaier">
+                <div class="columns is-centered">
+                    <div>
+
+                    </div>
+                </div>
+                </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Experience;
